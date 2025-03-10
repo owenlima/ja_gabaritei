04f0db52798481eecd4cddbb34b1ad81055f20ef
owenlima <owen.liima@gmail.com>
2025-03-10 11:27:40 -0300

    criação do backoffice e ajuste no sistema de layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get "welcome/index"
+  end
+  namespace :users_backoffice do
+    get "welcome/index"
+  end
+  namespace :admins_backoffice do
+    get "welcome/index"
+  end
   devise_for :users
   devise_for :admins
   get "welcome/index"
 
-  root to: "welcome#index"
+  root to: "site/welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
